4f05c047616a9e201515ef167eb7a04033a68dda
yangbiao <yangbiao@didiglobal.com>
2022-11-21 20:13:53 +0800

    feat: base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'TypeScript4 文档',
   description: 'TypeScript4 最新官方文档翻译',
-  base: '/vuepress-blog/',
+  base: '/vuepress-blog-dist/',
   theme: 'reco',
   locales: {
     '/': {
